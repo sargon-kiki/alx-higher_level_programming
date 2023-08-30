fc5cdbacbc1a892c397b82c7a973074bd5e9a68f
sargon-kiki <kikinem2016@gmail.com>
2023-08-30 04:05:45 +0000

    first project in classes in python

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0-square.py
+
+"""My Square Class"""
+
+
+class Square:
+    """Empty class square."""
+
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 1-square.py
+
+"""First Square Class"""
+
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
