30508c5e6ea469607be13e27de0d23d0f8ef29f3
Sargon Kiki <kikinem2016@gmail.com>
2023-09-04 22:40:17 -0700

    understanding more classes in python

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+
+"""This is the Rectangle class definition."""
+
+
+class Rectangle:
+    """Represents a rectangle object."""
+
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,4 @@
+Understanding python classes
+from using private instance attributes
+and public attributes as well
+This is part of the alx course program
